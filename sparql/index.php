0dc5d0c8d5eb53afd51f06356f530514d0db86dd
AlasdairGray <A.Gray@cs.man.ac.uk>
2012-10-10 15:08:46 +0100

    Enabled global variables for configuring endpoint URI and QueryExpander URI

--- sparql/index.php
+++ sparql/index.php
@@ -1,8 +1,21 @@
 <?php
+#$ENDPOINT = "http://localhost:8890/sparql/";
+$ENDPOINT = "http://cspc016.cs.man.ac.uk:8080/openrdf-sesame/repositories/OPS";
+$IS_VIRTUOSO = false;
+$DEBUG = false;
+#$QUERY_EXPANDER_URI = "http://openphacts.cs.man.ac.uk:9090/QueryExpander/";
+$QUERY_EXPANDER_URI = "http://cspc017.cs.man.ac.uk:8080/QueryExpander/";
 
 require_once "ops_ims.class.php";
-//require_once "virtuosoformatter.class.php";
+require_once "virtuosoformatter.class.php";
 
+if ( $DEBUG ) {
+	echo "<h2>Configuration</h2><ul>";
+        echo "<li>Endpoint URI: $ENDPOINT</li>";
+        echo "<li>Is virtuoso: $IS_VIRTUOSO</li>";
+	echo "<li>Query Expander URI: $QUERY_EXPANDER_URI</li>";
+        echo "</ul>";
+}
 if ( isset($_POST['query']) && !isset($_GET['query'])) {
 	runQuery($_POST['query']);
 }
@@ -17,11 +30,10 @@ else {
 }
 
 function runQuery ( $query ) {
-//	echo "<h2>Processing query:</h2><textarea readonly style=\"width:100%;\" rows=\"7\">$query</textarea>";
-	//$VirtuosoEndpoint = "http://localhost:8890/sparql/";
-	$SesameEndpoint = "http://cspc016.cs.man.ac.uk:8080/openrdf-sesame/repositories/OPS";
-	$ims = new OpsIms();
-	//$formatter = new VirtuosoFormatter();
+	global $ENDPOINT, $IS_VIRTUOSO, $DEBUG;
+	if ( $DEBUG ) {
+		echo "<h2>Processing query</h2><textarea readonly style=\"width:100%;\" rows=\"7\">$query</textarea>";
+	}
 	$inputURI='';
 	if (isset($_POST['inputURI'])) {
 		$inputURI = $_POST['inputURI'];
@@ -34,18 +46,29 @@ function runQuery ( $query ) {
 		$inputURI = $matches[1];
 		$query = preg_replace('/\[\][ ]*ops:input.*>[ ]*\./', '', $query);
 	}
+	$ims = new OpsIms();
 	$expanded_query = $ims->expandQuery($query, $inputURI);
-//	echo "<h2>Expanded query:</h2><textarea readonly style=\"width:100%;\" rows=\"7\">$expanded_query</textarea>";
-	//$virtuoso_query = $formatter->formatQuery($expanded_query);
-	//$con = curl_init($VirtuosoEndpoint);
-	$con = curl_init($SesameEndpoint);
+	if ( $DEBUG ) {
+		echo "<h2>Expanded Query</h2><textarea readonly style=\"width:100%;\" rows=\"20\">$expanded_query</textarea>";
+	}
+	if ( $IS_VIRTUOSO ) {
+		$formatter = new VirtuosoFormatter();
+		$final_query = $formatter->formatQuery($expanded_query);
+	} else {
+		$final_query = $expanded_query;
+	}
+	if ( $DEBUG ) {
+		echo "<h2>Final Query</h2><textarea readonly style=\"width:100%;\" rows=\"20\">$final_query</textarea>";
+	}
+	$con = curl_init($ENDPOINT);
 	curl_setopt($con, CURLOPT_POST, 1);
 	//Need to explicitly state the accept type for Sesame
 	curl_setopt($con, CURLOPT_HTTPHEADER, array("Accept: application/sparql-results+xml"));
-	//curl_setopt($con, CURLOPT_POSTFIELDS, array('query' => $virtuoso_query) );
-	curl_setopt($con, CURLOPT_POSTFIELDS, 'query='.$expanded_query );
+	curl_setopt($con, CURLOPT_POSTFIELDS, 'query='.$final_query );
 	curl_setopt($con, CURLOPT_RETURNTRANSFER, true);
-//	echo "<h2>Results</h2>";
+	if ( $DEBUG ) {
+		echo "<h2>Results</h2>";
+	}
 	echo curl_exec($con);
 	curl_close($con);
 }
