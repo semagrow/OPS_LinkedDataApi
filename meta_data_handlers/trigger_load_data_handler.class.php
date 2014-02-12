483fa0e35bf39b527c312519e7a8b4b12f0421cb
fundatureanu-sever <fundatureanu.sever@gmail.com>
2014-02-12 18:23:34 +0100

    Actual functionality

---
+++ meta_data_handlers/trigger_load_data_handler.class.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'data_handlers/data_handler_adapter.class.php';
+
+class TriggerLoadDataHandler extends DataHandlerAdapter{
+    
+    protected $Request = false;
+    protected $DataGraph = false;
+    private $pageUri = false;
+    private $Response;
+    
+    function __construct($dataHandlerParams){
+        $this->Request = $dataHandlerParams->Request;
+        $this->DataGraph = $dataHandlerParams->DataGraph;
+        $this->Response = $dataHandlerParams->Response;
+    } 
+    
+    function processData(){    
+        shell_exec("/var/www/html/scripts/loadingScript.sh &");
+        
+        $this->pageUri = $this->Request->getUriWithoutPageParam();
+        $this->DataGraph->add_literal_triple($this->pageUri, OPS_RESULT_PREDICATE, "Loading triggered successfully. Use /loadingStatus for progress info.");
+        
+        $this->Response->cacheable = NOT_CACHEABLE;
+    }
+    
+    function getPageUri(){
+        return $this->pageUri;
+    }
+    
+}
+
+?>
