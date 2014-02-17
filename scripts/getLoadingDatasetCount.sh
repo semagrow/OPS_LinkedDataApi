8729147bfcecca94bc1a77af570891b7f7af5cc9
vagrant <vagrant@precise64.(none)>
2014-02-17 12:46:43 +0000

    Fixed getLoadingDatasetsCount

--- scripts/getLoadingDatasetCount.sh
+++ scripts/getLoadingDatasetCount.sh
@@ -3,10 +3,10 @@
 SERVER_NAME="localhost"
 metaGraphName="http://www.openphacts.org/api/datasetDescriptorsTest"
 
-DatasetsLoading="SELECT COUNT(?description) WHERE { GRAPH <$metaGraphName>
+DatasetsLoadingQuery="SELECT COUNT(?description) WHERE { GRAPH <$metaGraphName>
 { ?description <http://www.openphacts.org/api#loadingStatus> <http://www.openphacts.org/api/LOADING> . }
 }"
-encodedQuery=$(php -r "echo urlencode(\"${dataDumpsQuery}\");")
+encodedQuery=$(php -r "echo urlencode(\"${DatasetsLoadingQuery}\");")
 DatasetsLoadingCount=$(curl "http://$SERVER_NAME:8890/sparql?query=$encodedQuery&format=csv" | tail -1)
-echo $DatasetsLoadingCount
+echo "$DatasetsLoadingCount"
 
