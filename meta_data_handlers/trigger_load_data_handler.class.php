f40301bac95a14cf6cff5443c468286ae4b43377
fundatureanu-sever <fundatureanu.sever@gmail.com>
2014-02-24 14:27:13 +0100

    Added a new API call for triggering linkset loading

--- meta_data_handlers/trigger_load_data_handler.class.php
+++ meta_data_handlers/trigger_load_data_handler.class.php
@@ -5,12 +5,14 @@ require_once 'data_handlers/data_handler_adapter.class.php';
 class TriggerLoadDataHandler extends DataHandlerAdapter{
     
     protected $Request = false;
+    protected $ConfigGraph = false;
     protected $DataGraph = false;
     private $pageUri = false;
     private $Response;
     
     function __construct($dataHandlerParams){
         $this->Request = $dataHandlerParams->Request;
+        $this->ConfigGraph = $dataHandlerParams->ConfigGraph;
         $this->DataGraph = $dataHandlerParams->DataGraph;
         $this->Response = $dataHandlerParams->Response;
     } 
@@ -23,7 +25,8 @@ class TriggerLoadDataHandler extends DataHandlerAdapter{
             $this->DataGraph->add_literal_triple($this->pageUri, OPS_RESULT_PREDICATE, "Loading not triggered. Other datasets are already loading. Try again later.");
         }
         else{
-            $cmd = "/var/www/html/scripts/loadingScript.sh datasets &> /dev/null &";
+            $loadingCommand = $this->ConfigGraph->get_first_literal($this->ConfigGraph->getEndpointUri(), API.'loadingCommand');
+            $cmd = $loadingCommand." &> /dev/null &";
             exec('/bin/bash -c "' . addslashes($cmd) . '"');
 
             $this->DataGraph->add_literal_triple($this->pageUri, OPS_RESULT_PREDICATE, "Loading triggered successfully. Use /loadingStatus for progress info.");
