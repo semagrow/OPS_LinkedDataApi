ab094cdeb85ab413808e173a6a43a11554388582
fundatureanu-sever <fundatureanu.sever@gmail.com>
2014-02-26 13:29:48 +0100

    Fixed triggering API calls to not allow running the cmd twice

--- meta_data_handlers/trigger_load_data_handler.class.php
+++ meta_data_handlers/trigger_load_data_handler.class.php
@@ -2,6 +2,8 @@
 
 require_once 'data_handlers/data_handler_adapter.class.php';
 
+define(CHECK_IF_CMD_RUNNING_SCRIPT, '/var/www/html/scripts/checkIfCommandIsRunning.sh');
+
 class TriggerLoadDataHandler extends DataHandlerAdapter{
     
     protected $Request = false;
@@ -20,12 +22,14 @@ class TriggerLoadDataHandler extends DataHandlerAdapter{
     function processData(){
         $this->pageUri = $this->Request->getUriWithoutPageParam();
         
-        $loadingDatasetsCount = shell_exec('/var/www/html/scripts/getLoadingDatasetCount.sh 2>/dev/null');
-        if ($loadingDatasetsCount > 0){
+        $loadingCommand = $this->ConfigGraph->get_first_literal($this->ConfigGraph->getEndpointUri(), API.'loadingCommand');
+        
+        $check = shell_exec(CHECK_IF_CMD_RUNNING_SCRIPT.' '.$loadingCommand.' 2>/dev/null');
+        if ($check == 'true'){
             $this->DataGraph->add_literal_triple($this->pageUri, OPS_RESULT_PREDICATE, "Loading not triggered. Other datasets are already loading. Try again later.");
         }
         else{
-            $loadingCommand = $this->ConfigGraph->get_first_literal($this->ConfigGraph->getEndpointUri(), API.'loadingCommand');
+            logDebug($loadingCommand." triggered");
             $cmd = $loadingCommand." &> /dev/null &";
             exec('/bin/bash -c "' . addslashes($cmd) . '"');
 
