94f440083f4c9087c80f2dec8d122a277df38957
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2016-07-02 12:05:05 +0300

    Fixes in the SPARQL extraction script
    Made using the executeSparql* scripts optinal
    Made the executeSparql* scripts executable
    Fixed the name of the md5 executable for MacOS
    Added some checks for robustness when creating directories deleting files

--- scripts/extract_sparql.sh
+++ scripts/extract_sparql.sh
@@ -3,11 +3,18 @@
 config_dir="$1"
 tmp_dir="$2"
 output_dir="$3"
+if [[ x$4 == x ]] ; then action=exec; else action="$4"; fi
+
+# the command to get the checksum is md5sum in Linux, md5 in MacOS
+checksum=$(which md5sum)
+if [[ "x$checksum" == "x" ]]; then checksum=$(which md5); fi
+if [[ "x$checksum" == "x" ]]; then checksum="noop"; fi
+
 if [[ `diff -N --exclude=*.sh --exclude=*.bak --exclude=*.json --exclude=".*" --exclude="colorCoding" --exclude="docs" "$config_dir" "$tmp_dir"` ]] ; then
     today=`date "+%Y%m%d"`
-    mkdir $output_dir/$today
+    mkdir -p $output_dir/$today
     echo "Changes detected, dumping SPARQL queries to: $output_dir/$today"
-    rm $tmp_dir/*.ttl
+    rm -f $tmp_dir/*.ttl
     cp $config_dir/*.ttl $tmp_dir/
     for file in $tmp_dir/*.ttl
     do
@@ -41,13 +48,13 @@ if [[ `diff -N --exclude=*.sh --exclude=*.bak --exclude=*.json --exclude=".*" --
 			selectQuery=`echo "$response" | sed -n '/^_:selectionQuery/,/""" ./p' | sed -e 's,_:selectionQuery rdf:value ,,' -e 's,"""[ \.]*,,'`
 			echo "$selectQuery" > $output_dir/$today/$filename.txt
 			outputFileName="$filename"_out
-			./executeSparqlSelector.sh "$selectQuery" > $output_dir/$today/$outputFileName
+			if [[ $action == exec ]] ; then ./executeSparqlSelector.sh "$selectQuery" > $output_dir/$today/$outputFileName; fi
 			blankNodeCount=$(grep "_:" $output_dir/$today/$outputFileName | wc -l)
 			grep -v "_:" $output_dir/$today/$outputFileName | sort >$output_dir/$today/temp
 			echo $blankNodeCount >>$output_dir/$today/temp
 
 			#sleep 30
-			cat $output_dir/$today/temp | md5sum | cut -d " " -f 1 >$output_dir/$today/$outputFileName.md5
+			cat $output_dir/$today/temp | $checksum | cut -d " " -f 1 >$output_dir/$today/$outputFileName.md5
 			rm $output_dir/$today/$outputFileName
 		fi
 		filename="$method_name"_view
@@ -61,14 +68,14 @@ if [[ `diff -N --exclude=*.sh --exclude=*.bak --exclude=*.json --exclude=".*" --
 		viewQuery=`echo "$response" | sed -n '/^_:viewingQuery/,/""" ./p' | sed -e 's,_:viewingQuery rdf:value ,,' -e 's,"""[ \.]*,,'`
 		echo "$viewQuery" > $output_dir/$today/$filename.txt
 		outputFileName="$filename"_out
-		./executeSparqlViewer.sh "$viewQuery" > $output_dir/$today/$outputFileName
+		if [[ $action == exec ]]; then ./executeSparqlViewer.sh "$viewQuery" > $output_dir/$today/$outputFileName; fi
 
 		blankNodeCount=$(grep "_:" $output_dir/$today/$outputFileName | wc -l)
                 grep -v "_:" $output_dir/$today/$outputFileName | sort >$output_dir/$today/temp
                 echo $blankNodeCount >>$output_dir/$today/temp
                 #sleep 30
 
-		cat $output_dir/$today/temp | md5sum | cut -d " " -f 1 >$output_dir/$today/$outputFileName.md5
+		cat $output_dir/$today/temp | $checksum | cut -d " " -f 1 >$output_dir/$today/$outputFileName.md5
 		rm $output_dir/$today/$outputFileName
 	done
     done
